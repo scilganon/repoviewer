c923f1e29a34c13c88f7794f0297034f4c539283
scilganon <scilganon@bigmir.net>
2019-07-22 09:32:18 +0300

    show exact 50 items

--- imports/actions/github.js
+++ imports/actions/github.js
@@ -5,6 +5,6 @@ let client = new GitHub();
 export const getCommits = () => {
     let repo = client.getRepo('vuejs/vue');
     return repo.listCommits({
-        since: '11.04.2019',
+        per_page: 50,
     })
 };
